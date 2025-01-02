5ff8f5fbf700eba504569d25ffb0296920df442b
Aditya Yadav <97823134+AdityaYadav777@users.noreply.github.com>
2025-01-02 10:28:13 +0530

    Merge remote-tracking branch 'origin/main'

--- home/src/main/java/com/renush/home/presentation/Home/TaskData.kt
+++ home/src/main/java/com/renush/home/presentation/Home/TaskData.kt
@@ -12,8 +12,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -45,17 +43,28 @@ fun TaskData() {
 }
 
 
-
 @Composable
 fun ShowData(taskDataModel: TaskDataModel) {
-    Card (modifier = Modifier.fillMaxWidth().padding(14.dp).height(80.dp), colors = CardDefaults.cardColors(Color.White), elevation = CardDefaults.cardElevation(7.dp)){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(14.dp)
+            .height(80.dp),
+        colors = CardDefaults.cardColors(Color.White),
+        elevation = CardDefaults.cardElevation(7.dp)
+    ) {
 
 
         Row {
 
             Box(
-                modifier = Modifier.size(80.dp).clip(RoundedCornerShape(topEnd = 10.dp, bottomEnd = 10.dp))
-                    .paint(painter = painterResource( R.drawable.taskbg), contentScale = ContentScale.FillBounds), contentAlignment = Alignment.Center
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(topEnd = 10.dp, bottomEnd = 10.dp))
+                    .paint(
+                        painter = painterResource(R.drawable.taskbg),
+                        contentScale = ContentScale.FillBounds
+                    ), contentAlignment = Alignment.Center
             ) {
                 Icon(
                     painter = painterResource(R.drawable.task),
@@ -65,14 +74,24 @@ fun ShowData(taskDataModel: TaskDataModel) {
                 )
             }
 
-            Column( modifier = Modifier.padding(start = 14.dp, top = 8.dp)) {
-                Text(taskDataModel.taskName, color = Color.Black, fontSize = 19.sp, fontWeight = FontWeight.Bold)
+            Column(modifier = Modifier.padding(start = 14.dp, top = 8.dp)) {
+                Text(
+                    taskDataModel.taskName,
+                    color = Color.Black,
+                    fontSize = 19.sp,
+                    fontWeight = FontWeight.Bold
+                )
                 Spacer(Modifier.height(4.dp))
                 Text(taskDataModel.taskSubmissionDate, color = Color.Black, fontSize = 13.sp)
 
             }
 
-            Row(modifier = Modifier.fillMaxWidth().padding(top = 10.dp, end = 6.dp), horizontalArrangement = Arrangement.End) { Text(taskDataModel.taskStatus, color = Color.Red, fontWeight = FontWeight.Bold)  }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, end = 6.dp),
+                horizontalArrangement = Arrangement.End
+            ) { Text(taskDataModel.taskStatus, color = Color.Red, fontWeight = FontWeight.Bold) }
 
         }
 
@@ -80,11 +99,12 @@ fun ShowData(taskDataModel: TaskDataModel) {
 }
 
 
-fun getTaskData():List<TaskDataModel>{
-    return listOf(TaskDataModel("Loading Page","submission date 15 Nov,2024","Pending"),
-        TaskDataModel("Dashboard Design","submission date 17 Nov,2024","Pending"),
-        TaskDataModel("Login/Sign Up Page","submission date 19 Nov,2024","Pending"),
-        TaskDataModel("Mobile App Ui Design","submission date 22 Nov,2024","Pending")
+fun getTaskData(): List<TaskDataModel> {
+    return listOf(
+        TaskDataModel("Loading Page", "submission date 15 Nov,2024", "Pending"),
+        TaskDataModel("Dashboard Design", "submission date 17 Nov,2024", "Pending"),
+        TaskDataModel("Login/Sign Up Page", "submission date 19 Nov,2024", "Pending"),
+        TaskDataModel("Mobile App Ui Design", "submission date 22 Nov,2024", "Pending")
     )
 }
 
